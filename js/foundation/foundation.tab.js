89e6a4e90b1772d281bc3ad48477140891ce9243
Max Bucknell <max.bucknell@redboxdigital.com>
2013-11-25 13:29:51 +0000

    Name all of the modules.
    Fastclick was defining its own module over the top of Foundation,
    so I've factored it out into its own module.

--- js/foundation/foundation.tab.js
+++ js/foundation/foundation.tab.js
@@ -1,45 +1,45 @@
-(function () {
-  var m = function ($, Foundation, undefined) {
-    'use strict';
+;(function () {
+var m = function ($, Foundation, undefined) {
+  'use strict';
 
-    Foundation.libs.tab = {
-      name : 'tab',
+  Foundation.libs.tab = {
+    name : 'tab',
 
-      version : '5.0.1',
+    version : '5.0.1',
 
-      settings : {
-        active_class: 'active'
-      },
+    settings : {
+      active_class: 'active'
+    },
 
-      init : function (scope, method, options) {
-        this.bindings(method, options);
-      },
+    init : function (scope, method, options) {
+      this.bindings(method, options);
+    },
 
-      events : function () {
-        $(this.scope).off('.tab').on('click.fndtn.tab', '[data-tab] > dd > a', function (e) {
-          e.preventDefault();
+    events : function () {
+      $(this.scope).off('.tab').on('click.fndtn.tab', '[data-tab] > dd > a', function (e) {
+        e.preventDefault();
 
-          var tab = $(this).parent(),
-              target = $('#' + this.href.split('#')[1]),
-              siblings = tab.siblings(),
-              settings = tab.closest('[data-tab]').data('tab-init');
+        var tab = $(this).parent(),
+            target = $('#' + this.href.split('#')[1]),
+            siblings = tab.siblings(),
+            settings = tab.closest('[data-tab]').data('tab-init');
 
-          tab.addClass(settings.active_class);
-          siblings.removeClass(settings.active_class);
-          target.siblings().removeClass(settings.active_class).end().addClass(settings.active_class);
-        });
-      },
+        tab.addClass(settings.active_class);
+        siblings.removeClass(settings.active_class);
+        target.siblings().removeClass(settings.active_class).end().addClass(settings.active_class);
+      });
+    },
 
-      off : function () {},
+    off : function () {},
 
-      reflow : function () {}
-    };
-
-    return Foundation;
+    reflow : function () {}
   };
 
+  return Foundation;
+};
+
   if (typeof define === 'function' && define.amd) {
-    define(['jquery', 'foundation'], m);
+    define('foundation/foundation.tab', ['jquery', 'foundation/foundation'], m);
   } else if (typeof Foundation !== 'undefined' && typeof jQuery !== 'undefined') {
     m(jQuery, Foundation);
   }
